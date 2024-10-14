99b8e5d9ea2e81e5170207f407f182bc4b31e100
Nguyen-Duy-Dang <nguyenduydang034@gmail.com>
2024-10-14 18:29:24 +0700

    DATN

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+use App\Http\Controllers\SinhVienController;
+use App\Http\Controllers\LopHocController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\db;
+use App\Http\Controllers\DemoController;
+use App\Http\Controllers\duydang;
+
+Route::get('/', function () {
+    return view('welcome');
+});
