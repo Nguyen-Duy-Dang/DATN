abe736042945ca6dadb8cebefaa2113237d62554
Nguyen-Duy-Dang <nguyenduydang034@gmail.com>
2024-10-14 18:57:33 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
-use App\Http\Controllers\SinhVienController;
-use App\Http\Controllers\LopHocController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\db;
-use App\Http\Controllers\DemoController;
-use App\Http\Controllers\duydang;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/admin');
-Route::get('/admin');
-Route::get('/admin');
